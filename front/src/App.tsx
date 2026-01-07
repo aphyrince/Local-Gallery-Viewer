36b2bb679ae0462bcaef385fe4a00927051701d3
aphyrince <dkswjdgh01@naver.com>
2026-01-07 09:13:29 +0900

    front 레이아웃 설정

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,9 +1,28 @@
+import styled from "styled-components";
+import ViewList from "./components/ViewList";
+
+const Wrapper = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background-image: url("./968864.png");
+    background-size: 100%;
+    color: white;
+    font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
+    font-size: large;
+    display: flex;
+    flex-direction: column;
+    justify-content: start;
+    align-items: start;
+    padding: 40px;
+    box-sizing: border-box;
+`;
+
 const App = () => {
     return (
-        <div>
+        <Wrapper>
             <h1>Local Gallery Viewer</h1>
-            <ul></ul>
-        </div>
+            <ViewList />
+        </Wrapper>
     );
 };
 
